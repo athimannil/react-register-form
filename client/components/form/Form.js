cbc9b002bfee865b806b298c72f24adb3d3a8a7d
Muhammed Athimannil <hello@athimannil.com>
2018-02-24 22:53:24 +0100

    more form inputs and font icon
    font icons from donatello.com

--- client/components/form/Form.js
+++ client/components/form/Form.js
@@ -7,7 +7,9 @@ export class Form extends React.Component {
         <div className="form-group">
           <label>First Name</label>
           <div className="input-group">
-            <span className="input-addon">X</span>
+            <span className="input-addon">
+              <i className="icon-user"></i>
+            </span>
             <input
               type="text"
               name="firstName"
@@ -16,6 +18,67 @@ export class Form extends React.Component {
             />
           </div>
         </div>
+
+        <div className="form-group">
+          <label>Last Name</label>
+          <div className="input-group">
+            <span className="input-addon">
+              <i className="icon-user"></i>
+            </span>
+            <input
+              type="text"
+              name="lastName"
+              className="input-field"
+              placeholder="Last name"
+            />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label>Username</label>
+          <div className="input-group">
+            <span className="input-addon">
+              <i className="icon-user"></i>
+            </span>
+            <input
+              type="text"
+              name="userName"
+              className="input-field"
+              placeholder="Username"
+            />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label>E-mail</label>
+          <div className="input-group">
+            <span className="input-addon">
+              <i className="icon-mail"></i>
+            </span>
+            <input
+              type="email"
+              name="email"
+              className="input-field"
+              placeholder="E-mail"
+            />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label>Password</label>
+          <div className="input-group">
+            <span className="input-addon">
+              <i className="icon-lock"></i>
+            </span>
+            <input
+              type="password"
+              name="password"
+              className="input-field"
+              placeholder="Password"
+            />
+          </div>
+        </div>
+
       </form>
     );
   }
--- client/components/social/Social.js
+++ client/components/social/Social.js
@@ -3,7 +3,9 @@ import React from 'react';
 export const Social = () => {
   return (
     <div className="register-social">
-      <a className="register-social-facebook" href="#">Register with Facebook</a>
+      <a className="register-social-facebook" href="#">
+        <i className="icon-facebook"></i>Register with Facebook
+      </a>
     </div>
   );
 };
